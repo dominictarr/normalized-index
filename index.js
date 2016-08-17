bf61ea7e8dd0df29b0375ebbf6ae6a1e0df7701b
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-18 10:32:52 +1200

    serialize an index

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var search = require('binary-search-async')
 var pull = require('pull-stream')
 
-module.exports = function (dir, log, compare, decode) {
+//this is an in-memory index, must be rebuilt from the log.
+module.exports = function (log, compare, decode) {
 
   //read the current max from the end of the start of the index file.
 
@@ -27,36 +28,29 @@ module.exports = function (dir, log, compare, decode) {
 
   function get (i, cb) {
     sort()
-    log.get(index[i].key, function (err, value) {
-      cb(null, decode(value))
-    })
+    cb(null, index[i].value, index[i].key, i)
   }
-
-  return {
+  var self
+  return self = {
+    length: function () { return index.length },
     get: get,
-
     search: function (target, cb) {
       sort()
       //we need to know the maximum value
       search(get, target, compare, 0, index.length, function (err, value, idx) {
-        cb(err, index[idx].key, value)
+        cb(err, value, index[idx < 0 ? ~idx : idx].key, idx)
       })
+    },
+    serialize: function () {
+      sort()
+      //format is <max_offset><ordered_offsets+>
+      var b = new Buffer((index.length+1)*4)
+      b.writeUInt32BE(max, 0)
+      index.forEach(function (e, i) {
+        b.writeUInt32BE(e.key, 4+i*4)
+      })
+      return b
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
