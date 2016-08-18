6605ee4a5a4c415dec3e88596c3d5f89a07c3dae
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-18 13:34:42 +1200

    run every test forward, backward, and with inclusive and exclusive ranges

--- test/stream.js
+++ test/stream.js
@@ -47,79 +47,88 @@ tape('alphabet', function (t) {
 
 })
 
-tape('everything', function (t) {
-  pull(
-    Stream(index, {}),
-    pull.collect(function (err, ary) {
+function all(opts, cb) {
+  pull(Stream(index, opts), pull.collect(cb))
+}
+
+function test(name, opts, fn) {
+  tape(name, function (t) {
+    all(opts, function (err, ary) {
       if(err) throw err
-      console.log(ary)
-      t.equal(ary.length, 24)
-      t.deepEqual(ary, ary.slice().sort(compare))
-      t.end()
+      fn(t, ary)
+      opts.keys = true
+      all(opts, function (err, with_keys) {
+        if(err) throw err
+        var vals = with_keys.map(function (e) { return e.value })
+        t.deepEqual(vals, ary)
+        fn(t, vals)
+        opts.values = false
+        all(opts, function (err, keys) {
+          t.deepEqual(keys, with_keys.map(function (e) { return e.key }))
+
+          opts.values = true
+          opts.keys = false
+          opts.reverse = true
+          all(opts, function (err, reverse_ary) {
+            if(err) throw err
+            fn(t, reverse_ary)
+            t.deepEqual(reverse_ary, ary.slice().reverse(), 'output correctly reversed')
+            t.end()
+          })
+        })
+      })
     })
-  )
-})
-tape('everything, reversed', function (t) {
-  pull(
-    Stream(index, {reverse: true}),
-    pull.collect(function (err, ary) {
-      if(err) throw err
-      console.log(ary)
-      t.equal(ary.length, 24)
-      t.deepEqual(ary, ary.slice().sort(compare).reverse())
-      t.end()
-    })
-  )
+  })
+}
+
+test('everthing', {}, function (t, ary) {
+  console.log(ary)
+  t.equal(ary.length, 24)
+//  t.deepEqual(ary, ary.slice().sort(compare))
 })
 
-tape('stream to half-way', function (t) {
-  var i = ~~(Math.random()*a.length)
-  var target = a[i]
-  pull(
-    Stream(index, {gte: target}),
-    pull.collect(function (err, ary) {
-      if(err) throw err
-      ary.forEach(function (e) {
-        t.ok(compare(e, target) >= 0, 
-          e.key + ' >= ' +target.key
-        )
-      })
-      t.end()
-    })
-  )
+//test('everything, reversed', {reverse: true}, function (t, ary) {
+//  t.equal(ary.length, 24)
+//  t.deepEqual(ary, ary.slice().sort(compare).reverse())
+//})
+
+var i = ~~(Math.random() * a.length)
+var target = a[i]
+
+test('stream to half-way', {gte: target}, function (t, ary) {
+  ary.forEach(function (e) {
+    t.ok(compare(e, target) >= 0,e.key + ' >= ' +target.key)
+  })
 })
 
-tape('stream middle range', function (t) {
-  var i = ~~(Math.random()*a.length)
-  var j = ~~(Math.random()*a.length)
-  var start = a[Math.min(i, j)]
-  var end = a[Math.max(i, j)]
-  pull(
-    Stream(index, {gt: start, lt: end}),
-    pull.collect(function (err, ary) {
-      if(err) throw err
-      ary.forEach(function (e) {
-        t.ok(compare(e, start) > 0, e.key+'>'+start.key)
-        t.ok(compare(e, end) < 0, e.key+'<'+end.key)
-      })
-      t.end()
-    })
-  )
-})
+for(var n = 0; n < 10; n++) (function () {
 
-tape('stream to half-way, reverse', function (t) {
-  var i = ~~(Math.random()*a.length)
-  var target = a[i]
-  pull(
-    Stream(index, {gte: target, keys: true, values: false}),
-    pull.collect(function (err, ary) {
-      if(err) throw err
-      console.log(ary)
-      ary.forEach(function (e) {
-        t.ok(compare(e, target) >= 0)
-      })
-      t.end()
+  var x = ~~(Math.random()*a.length)
+  var y = ~~(Math.random()*a.length)
+  var start = a[Math.min(x, y)]
+  var end = a[Math.max(x, y)]
+
+  test('stream middle range:'+start.key +'<'+end.key, {gt: start, lt: end}, function (t, ary) {
+    ary.forEach(function (e) {
+      t.ok(compare(e, start) > 0, e.key+'>'+start.key)
+      t.ok(compare(e, end) < 0, e.key+'<'+end.key)
     })
-  )
-})
+  })
+
+  test('stream middle range, end inclusive:'+start.key +'<='+end.key, {gt: start, lte: end}, function (t, ary) {
+    ary.forEach(function (e) {
+      t.ok(compare(e, start) > 0, e.key+'>'+start.key)
+      t.ok(compare(e, end) <= 0, e.key+'<='+end.key)
+    })
+  })
+
+  test('stream middle range, start inclusive:'+start.key +'=<'+end.key,
+  {gte: start, lt: end}, function (t, ary) {
+    ary.forEach(function (e) {
+      t.ok(compare(e, start) >= 0, e.key+'>='+start.key)
+      t.ok(compare(e, end) < 0, e.key+'<'+end.key)
+    })
+  })
+
+})()
 
