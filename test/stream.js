4a9192c3d9fbd246f9efaf9a33cd437730a182cc
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-19 14:24:54 +1200

    test inbetween streams, and test merged streams

--- test/stream.js
+++ test/stream.js
@@ -11,6 +11,7 @@ var dir = '/tmp/test-normalized-index_'+Date.now()
 mkdirp.sync(dir)
 
 var log = Offset(dir+'/log')
+
 function encode (value) {
   return new Buffer(JSON.stringify(value))
 }
@@ -23,7 +24,7 @@ function compare (a, b) {
   return a.key < b.key ? -1 : a.key > b.key ? 1 : 0
 }
 var index = Index(log, compare, decode)
-var table
+var table, table2
 
 var alpha = 'abcdefghijklmnopqrstuvwxyz'
 var a = []
@@ -37,25 +38,39 @@ tape('alphabet', function (t) {
     return Math.random() - 0.5
   })
 
+  var N = 10 + ~~(Math.random()*10)
   var i = 0
   ;(function loop () {
-    console.log(a[i])
+    if(i === N) {
+      table2 = IndexTable(index.serialize(), log, compare, decode)
+      index2 = Index(log, compare, decode, false)
+    }
     if(i === a.length) t.end()
     else log.append(encode(a[i++]), loop)
   })()
-
-  console.log(index.serialize())
 
 })
 
 tape('create table', function (t) {
   table = IndexTable(index.serialize(), log, compare, decode)
-  t.end()
+
+  console.log('index.length', index.length())
+  console.log('table.length', table.length())
+  console.log('index2.length', index2.length())
+  console.log('table2.length', table2.length())
+
+  all(table2, {}, function (err, ary) {
+    console.log('table2', ary)
+    all(index2, {}, function (err, ary) {
+      console.log('index2', ary)
+      t.end()
+    })
+  })
 })
 
 
 function all(index, opts, cb) {
-  pull(Stream(index, opts), pull.collect(cb))
+  pull(Stream(index, opts, compare), pull.collect(cb))
 }
 
 function error(err) {
@@ -79,14 +94,23 @@ function test(name, _opts, fn) {
             if(err) throw error(err)
             var vals = with_keys.map(function (e) { return e.value })
             t.deepEqual(vals, ary)
-            fn(t, vals)
+
+            t.ok(with_keys.every(function (e) {
+              return ~~e.key == e.key //is integer
+            }), 'keys are integers')
+
+          fn(t, vals)
             opts.values = false
             all(index, opts, function (err, keys) {
               t.deepEqual(keys, with_keys.map(function (e) { return e.key }))
+              t.ok(keys.every(function (e) {
+                return ~~e == e //is integer
+              }), 'keys are integers')
 
               opts.values = true
               opts.keys = false
               opts.reverse = true
+
               all(index, opts, function (err, reverse_ary) {
                 if(err) throw error(err)
                 fn(t, reverse_ary)
@@ -100,30 +124,63 @@ function test(name, _opts, fn) {
     }
     tests('mem:', index)
     tests('table:', table)
+    tests('merge:', [table2, index2])
   })
 }
 
 test('everthing', {}, function (t, ary) {
-  console.log(ary)
   t.equal(ary.length, 24)
 })
 
 var i = ~~(Math.random() * a.length)
 var target = a[i]
 
-test('stream to half-way', {gte: target}, function (t, ary) {
-  ary.forEach(function (e) {
-    t.ok(compare(e, target) >= 0,e.key + ' >= ' +target.key)
-  })
+
+test('empty stream, before start, closed', {gt: {key:'!'}, lt: {key:'!!'}}, function (t, ary) {
+  if(ary.length) throw new Error('should be empty')
+})
+
+test('empty stream, before start, open', {lt: {key:'!!'}}, function (t, ary) {
+  if(ary.length) throw new Error('should be empty')
+})
+
+
+test('empty stream, lte start, open', {lte: {key:'!!'}}, function (t, ary) {
+  if(ary.length) throw new Error('should be empty')
+})
+
+
+
+test('empty stream, before after end, closed', {gt: {key:'~'}, lt: {key:'~~'}}, function (t, ary) {
+  if(ary.length) throw new Error('should be empty')
+})
+
+test('empty stream, before after end, open', {gt: {key:'~'}}, function (t, ary) {
+  if(ary.length) throw new Error('should be empty')
+})
+
+test('empty stream, >= end, open', {gte: {key:'~'}}, function (t, ary) {
+  console.log(ary)
+  if(ary.length) throw new Error('should be empty')
+})
+
+
+test('stream to half-way : >='+target.key, {gte: target}, function (t, ary) {
+  t.ok(ary.every(function (e) {
+    if(!(compare(e, target) >= 0)) throw new Error(e.key+'>='+target.key)
+    return true
+  }))
+
 })
 
 var min = {key: '!'}, max = {key: '~'}
 test('stream part middle range, start inclusive:!=<~',
 { gte: min, lt: max}, function (t, ary) {
-  ary.forEach(function (e) {
-    t.ok(compare(e, min) >= 0, e.key+'>'+min.key)
-    t.ok(compare(e, max) < 0, e.key+'<'+max.key)
-  })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, min) >= 0)) throw new Error(e.key+'>='+min.key)
+      if(!(compare(e, max) < 0)) throw new Error(e.key+'<'+max.key)
+      return true
+    }))
 })
 
 for(var n = 0; n < 10; n++) (function () {
@@ -134,25 +191,26 @@ for(var n = 0; n < 10; n++) (function () {
   var end = a[Math.max(x, y)]
 
   test('stream middle range:'+start.key +'<'+end.key, {gt: start, lt: end}, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) > 0, e.key+'>'+start.key)
-      t.ok(compare(e, end) < 0, e.key+'<'+end.key)
-    })
+    t.ok(ary.every(function (e) {
+      return compare(e, start) > 0 && compare(e, end) < 0
+    }), 'within range')
   })
 
   test('stream middle range, end inclusive:'+start.key +'<='+end.key, {gt: start, lte: end}, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) > 0, e.key+'>'+start.key)
-      t.ok(compare(e, end) <= 0, e.key+'<='+end.key)
-    })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, start) > 0)) throw new Error(e.key+'>'+start.key)
+      if(!(compare(e, end) <= 0)) throw new Error(e.key+'<='+end.key)
+      return true
+    }))
   })
 
   test('stream middle range, start inclusive:'+start.key +'=<'+end.key,
   {gte: start, lt: end}, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) >= 0, e.key+'>='+start.key)
-      t.ok(compare(e, end) < 0, e.key+'<'+end.key)
-    })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, start) >= 0)) throw new Error(e.key+'>='+start.key)
+      if(!(compare(e, end) < 0)) throw new Error(e.key+'<'+end.key)
+      return true
+    }))
   })
 
   var s_part = { key: start.key.substring(0, 2)}
@@ -160,39 +218,40 @@ for(var n = 0; n < 10; n++) (function () {
 
   test('stream part middle range, start inclusive:'+s_part.key +'=<'+end.key,
   { gte: s_part, lt:  end }, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) >= 0, e.key+'>='+s_part.key)
-      t.ok(compare(e, end) < 0, e.key+'<'+end.key)
-    })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, start) >= 0)) throw new Error(e.key+'>='+start.key)
+      if(!(compare(e, end) < 0)) throw new Error(e.key+'<'+end.key)
+      return true
+    }))
   })
 
   test('stream part middle range, end inclusive:'+s_part.key +'=<'+end.key,
   { gt: s_part, lte:  end }, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) > 0, e.key+'>'+s_part.key)
-      t.ok(compare(e, end) <= 0, e.key+'<'+end.key)
-    })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, s_part) > 0)) throw new Error(e.key+'>'+s_part.key)
+      if(!(compare(e, end) <= 0)) throw new Error(e.key+'<='+end.key)
+      return true
+    }))
   })
 
   test('stream part middle range, end inclusive:'+start.key +'<='+e_part.key,
   { gt: start, lte:  e_part }, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) > 0, e.key+'>'+start.key)
-      t.ok(compare(e, end) <= 0, e.key+'<'+e_part.key)
-    })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, start) > 0)) throw new Error(e.key+'>'+start.key)
+      if(!(compare(e, e_part) <= 0)) throw new Error(e.key+'<='+e_part.key)
+      return true
+    }))
+
   })
 
   test('stream part middle range, start inclusive:'+start.key +'=<'+e_part.key,
   { gte: start, lt: e_part }, function (t, ary) {
-    ary.forEach(function (e) {
-      t.ok(compare(e, start) >= 0, e.key+'>'+start.key)
-      t.ok(compare(e, end) < 0, e.key+'<'+e_part.key)
-    })
+    t.ok(ary.every(function (e) {
+      if(!(compare(e, start) >= 0)) throw new Error(e.key+'>='+start.key)
+      if(!(compare(e, e_part) < 0)) throw new Error(e.key+'<'+e_part.key)
+      return true
+    }))
   })
-
 })()
-
-
-
 
 
