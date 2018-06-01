c6bceac9d30a644ca87076f2d3468e6f14bc3cfb
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-02 00:43:28 +1200

    benchmarks against flumeview-level

---
+++ test/bench.js
@@ -0,0 +1,78 @@
+var fs = require('fs')
+var tape = require('tape')
+var pull = require('pull-stream')
+var path = require('path')
+var Flume = require('flumedb')
+var FlumeLog = require('flumelog-offset')
+var FlumeViewLevel = require('flumeview-level')
+var json = require('flumecodec/json')
+var Index = require('../')
+var IndexTable = require('../table')
+
+var mkdirp = require('mkdirp')
+var rmrf = require('rimraf')
+
+var dir = '/tmp/test-normalized-index'
+
+var N = 10000
+
+var string_codec = {
+  decode: function (b) { return b.toString('ascii') }
+}
+
+var log = FlumeLog(path.join(dir, 'offset.log'), {codec: json})
+var db = Flume(log).use('level', FlumeViewLevel(1, function (value) {
+  return [value.hash]
+}))
+
+function pluck (raw) {
+//  console.log(raw, /([a-zA-Z0-9\+\/]+\=)/.exec(raw))
+  return /([a-zA-Z0-9\+\/]+=)/.exec(raw)[1]
+}
+
+function _compare (a, b) {
+  var h = pluck(a)
+//  console.log('compare', h)
+  return h < b.hash ? -1 : h > b.hash ? 1 : 0
+}
+
+function compare (a, b) {
+  return a.hash < b.hash ? -1 : a.hash > b.hash ? 1 : 0
+}
+
+var crypto = require('crypto')
+
+function hash (s) {
+  return crypto.createHash('sha256').update(s.toString()).digest('base64')
+}
+
+var raw_table = fs.readFileSync(path.join(dir, 'table'))
+
+var table = IndexTable(raw_table, log, compare)
+
+var target = {hash: hash(+process.argv[2])}
+var target2 = {hash: hash(+process.argv[3])}
+
+console.log('targets', target.hash, target2.hash)
+
+var targets = process.argv.slice(2).map(hash)
+
+;(function next () {
+  var start = Date.now()
+//  db.level.get(target.hash, function (err, value) {
+  //  console.log('level', Date.now()-start, value)
+    if(!targets.length) return
+    
+    start = Date.now()
+    var target = {hash: hash(+process.argv[2])}
+    table.search({hash: targets.shift()}, function (err, value, offset) {
+//    db.level.get(targets.shift(), function (err, value, offset) {
+      console.log('ni', Date.now()-start, value, offset)
+      next()
+    })
+  //})
+
+})()
+
+
+
---
+++ test/setup.js
@@ -0,0 +1,71 @@
+var fs = require('fs')
+var tape = require('tape')
+var pull = require('pull-stream')
+var path = require('path')
+var Flume = require('flumedb')
+var FlumeLog = require('flumelog-offset')
+var FlumeViewLevel = require('flumeview-level')
+var json = require('flumecodec/json')
+var Index = require('../')
+var IndexTable = require('../table')
+
+var mkdirp = require('mkdirp')
+var rmrf = require('rimraf')
+
+var dir = '/tmp/test-normalized-index'
+rmrf.sync(dir); mkdirp.sync(dir)
+
+var N = 1000
+
+var log = FlumeLog(path.join(dir, 'offset.log'), {codec: json})
+
+var db = Flume(log).use('level', FlumeViewLevel(1, function (value) {
+  return [value.hash]
+}))
+
+//var level = require('level')
+//var db = level(path.join(dir, 'level'), {valueEncoding: 'json'})
+
+function compare (a, b) {
+  return a.hash < b.hash ? -1 : a.hash > b.hash ? 1 : 0
+}
+
+var crypto = require('crypto')
+
+function hash (s) {
+//  return s
+  return crypto.createHash('sha256').update(s.toString()).digest('base64')
+}
+
+var index = Index(compare)
+
+pull(
+  log.stream({live: true}),
+  pull.drain(function (data) {
+    index.add({key:data.seq, value: data.value})
+  })
+)
+
+var j = 0
+;(function next (j) {
+  if(j > N) return next2()
+  console.log(j/N)
+  var a = [], b = []
+  for(var i = 0; i<N;i ++) {
+    var value = {hash: hash(i+(j*N)), time:Date.now(), i: i, j:j}
+    a.push(value)
+//    b.push({key: value.hash, value: value, type: 'put'})
+  }
+//  db.batch(b, function (err) {
+//    if(err) throw err
+    log.append(a, function () {
+      next(j + 1)
+    })
+//  })
+})(0)
+
+function next2 () {
+  console.log('loaded')
+  fs.writeFileSync(path.join(dir, 'table'), index.serialize())
+}
+
