1da9b68a4de3bf00f5c05a49c742ef47a781234e
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-02 01:22:24 +1200

    correct binary-search-async signature

--- file-table.js
+++ file-table.js
@@ -1,25 +1,22 @@
 'use strict'
 var search = require('binary-search-async')
-var Blocks = require('block-reader')
+var Blocks = require('aligned-block-file')
 
-module.exports = function (file, log, compare, decode) {
-  console.log(file)
+module.exports = function (file, log, compare) {
   var blocks = Blocks(file, 1024)
   var max = 0
 
   function offset(index, cb) {
+    if(isNaN(index)) throw new Error('index was NaN')
     blocks.readUInt32BE(4+index*4, cb)
   }
 
   function get (i, cb) {
-    setImmediate(function () {
-      offset(i, function (err, key) {
+    offset(i, function (err, key) {
+      if(err) return cb(err)
+      log.get(key, function (err, value) {
         if(err) return cb(err)
-        log.get(key, function (err, value) {
-          if(err) return cb(err)
-          value = decode(value)
-          cb(null, value, key)
-        })
+        cb(null, value, key)
       })
     })
   }
@@ -28,27 +25,23 @@ module.exports = function (file, log, compare, decode) {
   return self = {
     get: get,
     length: function () {
-      return (blocks.size()/4) - 1
+      return (blocks.size()-4)/4
     },
     search: function (target, cb) {
       //we need to know the maximum value
-      search(get, target, compare, 0, self.length(), function (err, value, i, exact) {
+      search(get, target, compare, 0, self.length()-1, function (err, i, value) {
         if(err) return cb(err)
-        if(i < 0)
-          cb(err, value, null, i, exact)
-        else {
+        if(i < 0) {
+          cb(err, value, null, i)
+        } else {
           offset(i, function (err, key) {
-            cb(err, value, key, i, exact)
+            cb(err, value, key, i)
           })
         }
       })
     }
   }
 }
-
-
-
-
 
 
 
