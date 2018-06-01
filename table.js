83f7e993102fe91d573bddfec4d7dbc75031c0ed
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-02 01:06:28 +1200

    handle some off-by-one errors

--- table.js
+++ table.js
@@ -7,7 +7,7 @@ module.exports = function (table, log, compare) {
     return table.readUInt32BE(4+index*4)
   }
 
-  var max = table.length/4 - 1
+  var max = (table.length-4)/4 - 1
 
   function get (i, cb) {
     setImmediate(function () {
@@ -18,14 +18,15 @@ module.exports = function (table, log, compare) {
   }
   return {
     get: get,
-    length: function () { return max },
+    length: function () { return max + 1},
     search: function (target, cb) {
       //we need to know the maximum value
-      search(get, target, compare, 0, max, function (err, idx, value, exact) {
+      search(get, target, compare, 0, max, function (err, idx, value) {
         cb(err, value, idx >= 0 ? offset(idx) : null, idx, idx >= 0)
       })
     }
   }
 }
+
 
 
