9c88cd937401155ec2d5794a559bf2a2b46b354f
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-02 00:42:34 +1200

    correct tests for inbetween

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@
 var tape = require('tape')
 var pull = require('pull-stream')
 
-var Offset = require('offset-log')
+var FlumeLog = require('flumelog-offset')
+var json = require('flumecodec/json')
 var Index = require('../')
 var IndexTable = require('../table')
 
@@ -11,7 +12,7 @@ var mkdirp = require('mkdirp')
 var dir = '/tmp/test-normalized-index_'+Date.now()
 mkdirp.sync(dir)
 
-var log = Offset(dir+'/log')
+var log = FlumeLog(dir+'/log', {codec: json})
 
 function compare (a, b) {
   return a.key < b.key ? -1 : a.key > b.key ? 1 : 0
@@ -19,43 +20,45 @@ function compare (a, b) {
 var index = Index(/*log, */compare, decode)
 
 pull(
-  log.stream({live: true, keys: true, value: true}),
-  pull.drain(index.add)
+  log.stream({live: true}),
+  pull.drain(function (data) {
+    index.add({key:data.seq, value: data.value})
+  })
 )
 
-function encode (value) {
-  return new Buffer(JSON.stringify(value))
-}
-
-function decode (value) {
-  return JSON.parse(value.toString())
-}
+function _id (e) { return e}
+var encode = _id
+var decode = _id
 
 tape('simple', function (t) {
-  log.append(encode({key: 'ABC', seq: 1}), function (err, _offset) {
+  var abc = {key: 'ABC', value: Math.random()}
+  log.append(abc, function (err, _offset) {
     if(err) throw err
     index.search({key: 'ABC'}, function (err, value, offset, i) {
       if(err) throw err
+      console.log(value, offset, i)
       t.equal(offset, _offset)
       t.equal(i, 0)
+      t.deepEqual(value, abc)
       t.end()
     })
   })
 })
 
 tape('more', function (t) {
-  log.append(encode({key: 'LMN', seq: 2}), function (err, offset1) {
+  var lmn = {key: 'LMN', random: Math.random()}
+  var xyz = {key: 'XYZ', random: Math.random()}
+  log.append(lmn, function (err, offset1) {
     if(err) throw err
-    log.append(encode({key: 'XYZ', seq: 3}), function (err, offset2) {
-      index.search({key: 'LMN'}, function (err, value, offset, i, exact) {
+    log.append(xyz, function (err, offset2) {
+      index.search({key: 'LMN'}, function (err, value, offset, i) {
         if(err) throw err
         console.log(offset, value)
         t.equal(offset, offset1, 'offsets are equal')
         t.equal(value.key, 'LMN')
         console.log('offset', offset)
-        t.equal(exact, true)
         log.get(offset, function (err, _value) {
-          t.deepEqual(value, decode(_value))
+          t.deepEqual(value, _value)
           t.end()
         })
       })
@@ -66,7 +69,7 @@ tape('more', function (t) {
 var alpha = 'abcdefghijklmnopqrstuvwxyz'
 var a = []
 for(var i = 0; i + 2 < alpha.length; i++)
-  a.push({key: alpha[i] + alpha[i+1] + alpha[i+2], seq: i+10})
+  a.push({key: alpha[i] + alpha[i+1] + alpha[i+2], index: i+10})
 
 tape('alphabet', function (t) {
 
@@ -76,9 +79,10 @@ tape('alphabet', function (t) {
   })
 
   var i = 0
-  ;(function loop () {
+  ;(function loop (err) {
+    if(err) throw err
     if(i === a.length) t.end()
-    else log.append(encode(a[i++]), loop)
+    else log.append(a[i++], loop)
   })()
 
 })
@@ -89,9 +93,10 @@ tape('search alphabet', function (t) {
     var i = 0
     ;(function loop () {
       if(i === a.length) return t.end()
-      console.log('search for:', target)
       var target = a[i++]
-      index.search(target, function (err, value) {
+      console.log('search for:', target)
+      index.search(target, function (err, value, i) {
+        console.log(err, value, i)
         t.deepEqual(value, target)
         loop()
       })
@@ -109,8 +114,9 @@ tape('serialize', function (t) {
       var target = a[i++]
       console.log('search for:', target)
       table.search(target, function (err, value, _, __, exact) {
+        if(err) throw err
         t.deepEqual(value, target)
-        t.equal(exact, true)
+//        t.equal(exact, true)
         loop()
       })
     })()
@@ -119,10 +125,9 @@ tape('serialize', function (t) {
 
 tape('partial', function (t) {
   var target = {key:'pq'}
-  index.search(target, function (err, value, offset, i, exact) {
+  index.search(target, function (err, value, offset, i) {
     if(err) throw err
     console.log(offset, value)
-    t.equal(exact, false)
     t.ok(compare(value, target) < 0, 'search returns value before target')
     index.get(~i+1, function (err, _value, _offset, j) {
       if(err) throw err
@@ -135,22 +140,36 @@ tape('partial', function (t) {
 })
 
 tape('out of bounds, high', function (t) {
-
   index.search({key: 'zzz'}, function (err, value, offset, i, exact) {
-    console.log('index', ~i, index.length())
+    console.log('index', ~i, index.length(), value, offset)
+    t.equal(~i, index.length())
+    console.log(err, value, offset, i)
+    t.end()
+  })
+})
+
+tape('out of bounds, inbetween', function (t) {
+  index.search({key: 'xxx'}, function (err, value, offset, i, exact) {
+    console.log('index', ~i, index.length(), value, offset)
     t.equal(~i, index.length()-1)
     console.log(err, value, offset, i)
     t.end()
   })
 })
-
-
-tape('out of bounds, low', function (t) {
-  index.search({key: '!'}, function (err, value, offset, i, exact) {
-    t.equal(i, -1)
-    console.log(err, value, offset, i)
+tape('out of bounds, inbetween', function (t) {
+  index.search({key: 'www'}, function (err, value, offset, i, exact) {
+    console.log('index', ~i, index.length(), value, offset)
+    t.equal(~i, index.length()-2)
     t.end()
   })
 })
 
+tape('out of bounds, low', function (t) {
+  index.search({key: '!'}, function (err, value, offset, i, exact) {
+    t.equal(i, -1)
+    t.equal(~i, 0)
+    console.log(err, value, offset, i)
+    t.end()
+  })
+})
 
